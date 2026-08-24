cd4eccfd1d7a0228dde9cc66190e871e6811121a
Bhumika Rawat <bhumikarawat8031@gmail.com>
2026-08-24 20:17:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (98.4%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char ch : s) {
+            // Push opening brackets
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            }
+            // Check closing brackets
+            else {
+                if (st.empty())
+                    return false;
+
+                if ((ch == ')' && st.top() == '(') ||
+                    (ch == ']' && st.top() == '[') ||
+                    (ch == '}' && st.top() == '{')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
